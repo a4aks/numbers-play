dd71c7fdbc4cd9d183cd88daec4f00021ceeb1ef
a4aks <a4ashish04@gmail.com>
2022-01-12 15:19:21 +0530

    Cube added

--- javascript/problem8.js
+++ javascript/problem8.js
@@ -1,5 +1,11 @@
 
-const num = [12,15,20,22,45];
+// const num = [12,15,20,22,45];
 
-const res = num.filter((x) => x%3 === 0);
-console.log(res)
+// const res = num.filter((x) => x%3 === 0);
+// console.log(res)
+
+const num = [2,4,5,8,10];
+
+const res = num.map(x => x*x*x);
+
+console.log(res);
